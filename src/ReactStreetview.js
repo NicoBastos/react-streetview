b0b1cf9b5d2d0eb02c5af50841ecdd5a64f17c65
nickbastos <nickbastos4@gmail.com>
2020-07-18 15:38:09 -0500

    Added a position prop to dynamically change the position of the StreetView panorama.

--- src/ReactStreetview.js
+++ src/ReactStreetview.js
@@ -1,58 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-import asyncLoading from 'react-async-loader';
+import asyncLoading from "react-async-loader";
 
 class ReactStreetview extends React.Component {
-
 	constructor() {
 		super();
 		this.streetView = null;
 	}
 
-	initialize (canvas) {
+	initialize(canvas) {
 		if (this.props.googleMaps && this.streetView == null) {
 			this.streetView = new this.props.googleMaps.StreetViewPanorama(
 				canvas,
 				this.props.streetViewPanoramaOptions
 			);
 
-			this.streetView.addListener('position_changed',() => {
+			this.streetView.addListener("position_changed", () => {
 				if (this.props.onPositionChanged) {
 					this.props.onPositionChanged(this.streetView.getPosition());
 				}
 			});
 
-			this.streetView.addListener('pov_changed',() => {
+			this.streetView.addListener("pov_changed", () => {
 				if (this.props.onPovChanged) {
 					this.props.onPovChanged(this.streetView.getPov());
 				}
 			});
 		}
-		
 	}
 
-	componentDidMount () {
+	componentDidMount() {
 		this.initialize(ReactDOM.findDOMNode(this));
 		this.streetView.setPosition(this.props.position);
 	}
 
-	componentDidUpdate () {
+	componentDidUpdate() {
 		this.initialize(ReactDOM.findDOMNode(this));
 	}
-	componentWillUnmount () {
+	componentWillUnmount() {
 		if (this.streetView) {
 			this.props.googleMaps.event.clearInstanceListeners(this.streetView);
 		}
 	}
 
-	render () {
-		return <div
-			style = {{
-				height: '100%'
-			}}
-		></div>;
+	render() {
+		if (this.streetView) {
+			this.streetView.setPosition(this.props.position);
+		}
+		return (
+			<div
+				style={{
+					height: "100%",
+				}}
+			></div>
+		);
 	}
 }
 
@@ -60,25 +63,26 @@ ReactStreetview.propTypes = {
 	apiKey: PropTypes.string.isRequired,
 	streetViewPanoramaOptions: PropTypes.object.isRequired,
 	onPositionChanged: PropTypes.func,
-	onPovChanged: PropTypes.func
+	onPovChanged: PropTypes.func,
+	position: PropTypes.object,
 };
 
 ReactStreetview.defaultProps = {
-	streetViewPanoramaOptions : {
-		position: {lat: 46.9171876, lng: 17.8951832},
-		pov: {heading: 0, pitch: 0},
-		zoom: 1
-	}
+	streetViewPanoramaOptions: {
+		pov: { heading: 0, pitch: 0 },
+		zoom: 1,
+	},
+	position: { lat: 46.9171876, lng: 17.8951832 },
 };
 
-function mapScriptsToProps (props) {
+function mapScriptsToProps(props) {
 	const googleMapsApiKey = props.apiKey;
 	return {
 		googleMaps: {
-			globalPath: 'google.maps',
-			url: 'https://maps.googleapis.com/maps/api/js?key=' + googleMapsApiKey,
-			jsonp: true
-		}
+			globalPath: "google.maps",
+			url: "https://maps.googleapis.com/maps/api/js?key=" + googleMapsApiKey,
+			jsonp: true,
+		},
 	};
 }
 
